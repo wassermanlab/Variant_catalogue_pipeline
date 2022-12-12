2a36b9f169576aa1b83d86f80b2e1538dafcdb73
Solenne Correard <54953390+scorreard@users.noreply.github.com>
2022-12-11 16:38:02 -0800

    Update Variant_Catalogue_v1.sh
    Changed the name of the nextflow pipeline to main.nf

--- Variant_Catalogue_v1.sh
+++ Variant_Catalogue_v1.sh
@@ -15,4 +15,4 @@ source /mnt/common/SILENT/Act3/conda/miniconda3/etc/profile.d/conda.sh
 Nextflow=/mnt/common/Precision/NextFlow/nextflow
 module load singularity
 
-$Nextflow run Variant_Catalogue_v1.nf -profile GRCh37 -resume -with-trace -with-report -with-timeline  -with-dag flowchart.png
+$Nextflow run main.nf -profile GRCh37 -resume -with-trace -with-report -with-timeline  -with-dag flowchart.png

2118f6b2d26a7108737ab30add912cf1764c1325
Phillip Richmond <prichmond@gpcc-node01.bcricwh.lan>
2023-05-01 14:36:25 -0700

    functional up to SNV and SV organization

--- Variant_Catalogue_v1.sh
+++ Variant_Catalogue_v1.sh
@@ -4,9 +4,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=4G
-#SBATCH --time=200:30:00
 #SBATCH -p silent_q
-#SBATCH --mail-user=melsiddieg@cmmt.ubc.ca
+#SBATCH --mail-user=prichmond@bcchr.ca
 # #SBATCH --mail-type=FAIL
 
 ## Output and Stderr
@@ -17,4 +16,4 @@ source /mnt/common/SILENT/Act3/conda/miniconda3/etc/profile.d/conda.sh
 Nextflow=/mnt/common/Precision/NextFlow/nextflow
 module load singularity
 prof=$1
-$Nextflow run main.nf -profile $prof -resume -with-trace -with-report -with-timeline  -with-dag flowchart.png
+$Nextflow run main.nf -profile GRCh37 -resume -with-trace -with-report -with-timeline  -with-dag flowchart.png

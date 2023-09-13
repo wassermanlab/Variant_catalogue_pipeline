55a67631d1a8f7bd2003ea413b255c35c012877d
Mohammed OE Abdallah <melsiddieg@users.noreply.github.com>
2023-09-13 11:48:19 -0700

    final fix for SNV R data organization script

--- modules/SNV_data_organization.R
+++ modules/SNV_data_organization.R
@@ -26,7 +26,11 @@ assembly=(args[1])
 chr = args[2]
 ####Organize different tables
 ##Read gnomAD SNV vcf
+<<<<<<< HEAD
+pat = glue("gnomad_frequency_table_(chr)?{chr}.tsv")
+=======
 pat = glue("gnomad_frequency_table_(chr)?{chr}")
+>>>>>>> bfd55523898b716ed6a2d97274c89af0eaaf65f2
 gfile = grep(perl = TRUE,pattern = pat,x = list.files(),value = T)
 gnomad_file=fread(gfile)
 if (length(gnomad_file)>11)gnomad_file[,12]=NULL
